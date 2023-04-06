9c784b22119612b2ac8f0650732d503a6a0fbce9
Kevillin Santos <kevillin_santos@hotmail.com>
2023-04-06 18:23:07 -0300

    feat: adiciona requisito 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,6 +1,14 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    # Seu teste deve ser escrito aqui
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        12, "Teste1", "Teste2", "Teste3", "Teste4", "Teste5", "Teste6"
+    )
+    assert produto.id == 12
+    assert produto.nome_do_produto == "Teste1"
+    assert produto.nome_da_empresa == "Teste2"
+    assert produto.data_de_fabricacao == "Teste3"
+    assert produto.data_de_validade == "Teste4"
+    assert produto.numero_de_serie == "Teste5"
+    assert produto.instrucoes_de_armazenamento == "Teste6"
