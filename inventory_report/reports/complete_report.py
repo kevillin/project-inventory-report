5f53871e9ee5325db15e84f9633a18851d2295cd
Kevillin Santos <kevillin_santos@hotmail.com>
2023-04-11 10:50:50 -0300

    feat: requisito 3 com ajuda do Carlos Augusto

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,18 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+
+    def generate(list) -> str:
+        empresa_com_mais_produto = {}
+        for item in list:
+            if item["nome_da_empresa"] in empresa_com_mais_produto:
+                empresa_com_mais_produto[item['nome_da_empresa']] += 1
+            else:
+                empresa_com_mais_produto[item['nome_da_empresa']] = 1
+
+        retorno_completo = "\nProdutos estocados por empresa:\n"
+        for key, value in empresa_com_mais_produto.items():
+            retorno_completo += f"- {key}: {value}\n"
+
+        return SimpleReport.generate(list) + retorno_completo
